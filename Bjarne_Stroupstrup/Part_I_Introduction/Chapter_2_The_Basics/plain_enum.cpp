1f1b11805059b2188ed542ca0ce0bbb81c08ecb3
jdighton <jdighton@debian-laptop1.lan>
2022-08-30 10:59:16 -0500

    notes and code

--- Bjarne_Stroupstrup/Part_I_Introduction/Chapter_2_The_Basics/plain_enum.cpp
+++ Bjarne_Stroupstrup/Part_I_Introduction/Chapter_2_The_Basics/plain_enum.cpp
@@ -1,12 +1,27 @@
 #include <iostream> 
 
-enum Color {
-    red, 
-    green, 
-    blue
-};
+enum class Color { red, blue, green };
+enum class Traffic_light { green, yellow, red };
+
+Color col = Color::red; 
+Traffic_light light = Traffic_light::red; 
+
+// this returns a reference to a Traffic_light, but isn't a member of the class
+// so how do we call this func?
+Traffic_light& operator++(Traffic_light& t){
+    // prefix increment
+    switch(t){
+        case Traffic_light::green:      return t=Traffic_light::yellow; 
+        case Traffic_light::yellow:     return t=Traffic_light::red; 
+        case Traffic_light::red:        return t=Traffic_light::green; 
+    }
+}
 
 int main(){
+    Traffic_light light { Traffic_light::red };
+    ++light; 
+
+    
 
     return 0; 
 }
